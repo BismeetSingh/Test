c0cd0d9774e38f7bcf05be27e2a97fbfce0d29c4
bismeet <bismeetsingh352@gmail.com>
2018-07-07 07:41:30 +0530

    Changed toast

--- app/src/main/java/com/app/bissudroid/test1/Test.java
+++ app/src/main/java/com/app/bissudroid/test1/Test.java
@@ -4,4 +4,5 @@ public class Test {
     private StringBuilder error;
     private int b;
     private int b1;
+    private int b4;
 }
--- app/src/main/java/com/app/bissudroid/test1/Test1.java
+++ app/src/main/java/com/app/bissudroid/test1/Test1.java
@@ -3,4 +3,5 @@ package com.app.bissudroid.test1;
 public class Test1 {
     private int c;
     private int c1;
+    private int c4;
 }
