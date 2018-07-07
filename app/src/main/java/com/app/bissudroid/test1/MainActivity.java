c5a2f5438485c40644e0e78362f00ae8b3126f5e
bismeet <bismeetsingh352@gmail.com>
2018-07-07 07:17:30 +0530

    Changed three activities.

--- app/src/main/java/com/app/bissudroid/test1/MainActivity.java
+++ app/src/main/java/com/app/bissudroid/test1/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "Hey this is test1 file changed by backuptest1", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Hey this is a test3toast", Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/app/bissudroid/test1/ThirdActivity.java
@@ -0,0 +1,5 @@
+package com.app.bissudroid.test1;
+
+public class ThirdActivity {
+    private StringBuilder errorStringBuilder;
+}
