cbf05a4bc8fda001d5f318a6ef86e997a08ddbc6
bismeet <bismeetsingh352@gmail.com>
2018-07-07 07:18:58 +0530

    Changed toast

--- app/src/main/java/com/app/bissudroid/test1/MainActivity.java
+++ app/src/main/java/com/app/bissudroid/test1/MainActivity.java
@@ -10,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toast.makeText(this, "Hey this is test1 file changed by backuptest1", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Hey this is a test2toast", Toast.LENGTH_SHORT).show();
     }
 }
